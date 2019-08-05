b7db15c78a09896d31aac8135e4628a61adb24c2
Richard Epure <richardvandick@gmail.com>
2019-08-05 20:19:09 +0100

    GameList now receives and displays input data

--- src/GameList.js
+++ src/GameList.js
@@ -3,33 +3,42 @@ import ReactDOM from 'react-dom';
 
 class GameList extends React.Component
 {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            items: props.items
-        }
-    }
-
     createItemList()
     {
         let itemList = [];
 
         for(let i=0; i<this.props.items; i++)
         {
-            itemList.push(<li>item {i+1}</li>);
+            itemList.push(
+                <li key={i} className="gamelist-item">
+                    <div className="row">
+                        <div className="col-auto">
+                                <img src="" alt="item image" className="gamelist-image"></img>
+                        </div>
+                        <div className="col-auto">
+                                {this.props.data[i].name}<br></br>
+                                {this.props.data[i].store}<br></br>
+                                {this.props.data[i].price}
+                        </div>
+                    </div>
+                </li>
+            );
         }
         return itemList;
     }
 
     render()
     {
-    
-
         return (
-            <ul>
-                {this.createItemList()}
-            </ul>
+            <div className="container gamelist-container">
+                <div class="row justify-content-center">
+                    <div class="col-auto">
+                        <ul className="gamelist">
+                            {this.createItemList()}
+                        </ul>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
