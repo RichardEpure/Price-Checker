606f776e4911ddb18a998a2e179c7d99318ce285
Richard Epure <richardvandick@gmail.com>
2019-08-06 17:47:17 +0100

    updated GameList Styling, added feature: focus on single list item by clicking on it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import GameList from './GameList'
 
 class App extends React.Component
 {
     render()
     {
-        const data = JSON.parse('[{"name" : "God of war", "store" : "SEEX", "price" : "0.99"},{"name" : "Eternal Quest", "store" : "unknown", "price" : "99.99"},{"name" : "CSGO", "store" : "steam", "price" : "6.99"}]');
+        const data = 
+        [
+            {
+                "name": "God of war",
+                "store": "SEEX",
+                "price": "0.99"
+            },
+            {
+                "name": "Eternal Quest",
+                "store": "unknown",
+                "price": "99.99"
+            },
+            {
+                "name": "CSGO",
+                "store": "steam",
+                "price": "6.99"
+            }
+        ];
 
         return (
             <GameList 
--- src/GameList.js
+++ src/GameList.js
@@ -1,40 +1,107 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import './index.css';
 
 class GameList extends React.Component
 {
-    createItemList()
+    constructor(props)
     {
-        let itemList = [];
+        super(props);
+        this.state = {
+            itemList: Array(this.props.items),
+            expandedItem: undefined
+        }
+    }
 
+    expandItem(key)
+    {
+        let newItemList = this.state.itemList.slice();
+
+        if(this.state.expandedItem !== undefined)
+        {
+            let eKey = this.state.expandedItem;
+
+            newItemList[eKey] =
+                <li key={eKey} className="gamelist-item">
+                    <button onClick={() => this.expandItem(eKey)}>
+                        <div className="row">
+                            <div className="col-auto">
+                                    <img src="" alt="game" className="gamelist-image"></img>
+                            </div>
+                            <div className="col-auto">
+                                    <h5>{this.props.data[eKey].name}</h5>
+                                    <h6>{this.props.data[eKey].store}</h6>
+                                    <h6>{this.props.data[eKey].price}</h6>
+                            </div>
+                        </div>
+                    </button>
+                </li>;
+        }
+
+        newItemList[key] =
+            <li key={key} className="gamelist-item-expanded">
+                <div className="row">
+                    <div className="col-auto">
+                            <img src="" alt="game" className="gamelist-image"></img>
+                    </div>
+                    <div className="col-auto">
+                            <h5>{this.props.data[key].name}</h5>
+                            <h6>{this.props.data[key].store}</h6>
+                            <h6>{this.props.data[key].price}</h6>
+                    </div>
+                </div>
+            </li>;
+
+        this.setState({
+            itemList: newItemList,
+            expandedItem: key
+        });
+    }
+
+    createList()
+    {
+        let newItemList = this.state.itemList.slice();
         for(let i=0; i<this.props.items; i++)
         {
-            itemList.push(
+            newItemList[i] =
                 <li key={i} className="gamelist-item">
-                    <div className="row">
-                        <div className="col-auto">
-                                <img src="" alt="item image" className="gamelist-image"></img>
+                    <button onClick={() => this.expandItem(i)}>
+                        <div className="row">
+                            <div className="col-auto">
+                                    <img src="" alt="game" className="gamelist-image"></img>
+                            </div>
+                            <div className="col-auto">
+                                    <h5>{this.props.data[i].name}</h5>
+                                    <h6>{this.props.data[i].store}</h6>
+                                    <h6>{this.props.data[i].price}</h6>
+                            </div>
                         </div>
-                        <div className="col-auto">
-                                {this.props.data[i].name}<br></br>
-                                {this.props.data[i].store}<br></br>
-                                {this.props.data[i].price}
-                        </div>
-                    </div>
-                </li>
-            );
+                    </button>
+                </li>;
         }
-        return itemList;
+
+        this.setState({
+            itemList: newItemList
+        });
+    }
+
+    componentDidUpdate()
+    {
+
+    }
+
+    componentDidMount()
+    {
+        this.createList();
     }
 
     render()
     {
         return (
             <div className="container gamelist-container">
-                <div class="row justify-content-center">
-                    <div class="col-auto">
+                <div className="row justify-content-center">
+                    <div className="col-12">
                         <ul className="gamelist">
-                            {this.createItemList()}
+                            {this.state.itemList}
                         </ul>
                     </div>
                 </div>
